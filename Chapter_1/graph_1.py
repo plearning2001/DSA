cc17eca2a794fe47d240de918fe60201bba1fec4
plearning2001 <p.learning2001@gmail.com>
2025-10-11 08:10:54 +0530

    First Graph

---
+++ Chapter_1/graph_1.py
@@ -0,0 +1,6 @@
+# Given vertices and edges
+V = [0,1,2,3,4]
+E = [(0, 1), (0, 2), (1, 3), (1, 4), (2, 4), (2, 3), (3, 4)] # each tuple(u,v) represent edge from u to v
+
+size = len(V)
+print(f"size of graph -- {size}")

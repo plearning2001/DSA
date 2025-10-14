02b98aa5de9c045f63681ff9825ec3cd9658ba94
plearning2001 <p.learning2001@gmail.com>
2025-10-15 01:24:37 +0530

    DAG

---
+++ DAG/longest_path_dag.py
@@ -0,0 +1,57 @@
+
+# Implementation of Queue
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self,v):
+        self.queue.append(v)
+    def isempty(self):
+        return(self.queue == [])
+    def dequeue(self):
+        v = None
+        if not self.isempty():
+            v = self.queue[0]
+            self.queue = self.queue[1:]
+        return(v)    
+    def __str__(self):
+        return(str(self.queue))
+
+# Implementation Longest path on DAG for Adjacency list
+def longestpathlist(AList):
+    # Initialization
+    (indegree,lpath) = ({},{})
+    zerodegreeq = Queue()
+    for u in AList.keys():
+        (indegree[u],lpath[u]) = (0,0)
+    
+    # Compute indegree for each vertex
+    for u in AList.keys():
+        for v in AList[u]:
+            indegree[v] = indegree[v] + 1
+
+    # Find the vertex with indegree 0 and added into the queue
+    for u in AList.keys():
+        if indegree[u] == 0:
+            zerodegreeq.enqueue(u)
+            
+    # Longest path computing process            
+    while (not zerodegreeq.isempty()):
+        # Remove one vertex from queue which have zero degree vertices and reduce the indegree by 1
+        curr_vertex = zerodegreeq.dequeue()
+        indegree[curr_vertex] = indegree[curr_vertex] - 1
+        
+        # Repeat for each adjacent of the removed vertex 
+        for adj_vertex in AList[curr_vertex]:
+            # Reduce the indegree of each adjacent of the removed vertex by 1
+            indegree[adj_vertex] = indegree[adj_vertex] - 1
+            # Assign the longest path value for each adjacent of the removed vertex
+            lpath[adj_vertex] = max(lpath[adj_vertex],lpath[curr_vertex]+1)
+            # If after reducing the degree of adjacent, it becomes zero then insert it into the queue
+            if indegree[adj_vertex] == 0:
+                zerodegreeq.enqueue(adj_vertex)
+                
+    return(lpath)
+
+
+AList={0: [2, 3, 4], 1: [2, 7], 2: [5], 3: [5, 7], 4: [7], 5: [6], 6: [7], 7: []}
+print(longestpathlist(AList))

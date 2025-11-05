8e80d96f218bc283d8f2b7a10bdf258b46883388
plearning2001 <p.learning2001@gmail.com>
2025-11-05 23:13:39 +0530

    Lambda function

---
+++ easy/add_one_to_array.py
@@ -0,0 +1,14 @@
+def add_one(arr,last_index):
+	if arr[last_index] != 9:
+		arr[last_index] += 1
+		return arr
+	arr[last_index] = 0
+	if (last_index*-1) == len(arr):
+		arr.insert(0,1)
+		return arr
+	return add_one(arr,last_index-1)
+			
+
+
+arr = [9,9,9]
+print(add_one(arr,-1))
--- easy/add_sq_of_natural_no.py
+++ easy/add_sq_of_natural_no.py
@@ -3,10 +3,15 @@ We  can use simple for loop or recursion O(n)
 But this solution will give result in constant time i.1 O(1)
 '''
 
-def add_all_sq(n):
-    value = (n*(n+1) * ((2*n) + 1)) /6
-    return int(value)
+# def add_all_sq(n):
+#     value = (n*(n+1) * ((2*n) + 1)) /6
+#     return int(value)
+
+'''
+Use of lambda function
+'''
+add_all_sq = lambda n: (n*(n+1) * ((2*n) + 1)) /6
 
 if __name__ == "__main__":
-    n = 1
+    n = 3
     print(f" Sum of squares is - {add_all_sq(n)}")

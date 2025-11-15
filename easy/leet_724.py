4d6a495def2bcef6a025fc92b170766fe02212d0
plearning2001 <p.learning2001@gmail.com>
2025-11-15 19:08:14 +0530

    leetcode 724 issue

--- easy/leet_724.py
+++ easy/leet_724.py
@@ -10,18 +10,23 @@ class Solution:
         print(right_sum)
 
         left_sum = 0
-        for j in nums:
-            if left_sum == right_sum-j:
-                return nums.index(j)
+        for j in range(len(nums)):
+            if left_sum == right_sum-nums[j]:
+                return j
             else:
-                left_sum = left_sum + j
-                right_sum = right_sum-j
+                left_sum = left_sum + nums[j]
+                right_sum = right_sum - nums[j]
         return -1
 
 
 answer_object = Solution()
 nums1 = [1,2,3]
 nums2 = [12,345,2,6,7896]
-nums = [2,1,-1]
+nums3 = [2,1,-1]
+nums4 = [-1,-1,-1,-1,-1,0]
+nums = [-1,-1,0,1,1,0]
+
+
+
 answer = answer_object.pivotIndex(nums)
 print(f"answer - {answer}")

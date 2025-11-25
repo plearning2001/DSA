8e932a87aa33cd107708bd5c7201a0f6e3182e22
plearning2001 <p.learning2001@gmail.com>
2025-11-25 20:50:49 +0530

    leetcode 704

---
+++ easy/leet_704.py
@@ -0,0 +1,22 @@
+nums = [-1,0,3,5,9,12]
+target = 9
+
+
+left = 0
+data = nums
+right = len(data) - 1
+        
+
+while left <= right:
+    middle = (left + right)//2
+    if data[middle] == target:
+        print(middle)
+    else:
+        if target > data[middle]:
+            left = middle+1
+        else:
+            right = middle-1
+if target == data[right]:
+    print right
+
+print -1

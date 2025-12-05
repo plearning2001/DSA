178546e4b7dde9114de7945f222db172e1298667
plearning2001 <p.learning2001@gmail.com>
2025-12-06 00:02:33 +0530

    Merged two sorted arrays in new array & remove duplicates edge case

--- easy/merge_two_sorted_in_new.py
+++ easy/merge_two_sorted_in_new.py
@@ -1,5 +1,6 @@
-a = [1,2,3,4,5,6,7,8,9]
+a = [1,2,2,3,4,5,6,7,8,9]
 b = [2,3,4,5,11]
+print(f"a - {a} \n b - {b}")
 
 #target = [1,2,3,4,5,6]
 
@@ -10,27 +11,33 @@ j=0
 len_a = len(a)
 len_b = len(b)
 
+def add_element(c,element):
+    if element in c:
+        return
+    else:
+        c.append(element)
+
 while i<len_a or j<len_b:
     if i>=len_a:
-        c.append(b[j])
+        add_element(c,b[j])
         j+=1
         continue
     if j>=len_b:
-        c.append(b[i])
+        add_element(c,b[i])
         i+=1
         continue    
 
     if a[i]<b[j]:
-        c.append(a[i])
+        add_element(c,a[i])
         i+=1
     elif a[i]>b[j]:
-        c.append(b[j])
+        add_element(c,b[j])
         j+=1
     
     
     #Remove duplicates
     else:
-        c.append(b[j])
+        add_element(c,b[j])
         j+=1
         i+=1
 

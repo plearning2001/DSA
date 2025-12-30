45311e9502f6eb4efb93f5eece6626a1971163a3
plearning2001 <p.learning2001@gmail.com>
2025-12-30 15:34:14 +0530

    repeated-dna-sequences approach 1

---
+++ Sliding_window/leet_187.py
@@ -0,0 +1,23 @@
+# repeated-dna-sequences
+
+s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+repeated_set = {}
+k = 10
+
+end_i = len(s)-k + 1
+
+for i in range(0,end_i):
+    # print(s[i:i+10])
+    seq = s[i:i+10]
+    if seq in repeated_set.keys():
+        repeated_set[seq] = repeated_set[seq] +1
+    else:
+        repeated_set[seq] = 1
+
+final_list = []
+for key,value in repeated_set.items():
+    if value>1:
+        final_list.append(key)
+
+print(final_list)
+

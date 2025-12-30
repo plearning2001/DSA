24212eb290415b2aeee50ea1381c3248a3bf2445
plearning2001 <p.learning2001@gmail.com>
2025-12-30 18:59:14 +0530

    contains-duplicate-ii

---
+++ Sliding_window/leet_219.py
@@ -0,0 +1,22 @@
+# contains-duplicate-ii
+
+nums = [1,2,3,1]
+k = 3
+nums = [1,0,1,1]
+k = 1
+
+nums = [1,2,3,1,2,3]
+k = 2
+
+dict = {}
+target = False
+for i in range(len(nums)):
+    if nums[i] in dict.keys():
+        if i - dict[nums[i]] <= k:
+            target = True
+        else:
+            dict[nums[i]] = i    
+    else:
+        dict[nums[i]] = i
+
+print(target)

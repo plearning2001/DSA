bc818f5f1cba5d9e111c0fad1a138593449f0a30
plearning2001 <p.learning2001@gmail.com>
2025-12-30 18:08:09 +0530

    longest-substring-without-repeating-characters

---
+++ Sliding_window/leet_3.py
@@ -0,0 +1,20 @@
+# longest-substring-without-repeating-characters
+
+s = "abcbbcbb"
+
+i = 0
+j = 0
+char_set = set()
+max_total = 0
+
+while j < len(s):
+    c = s[j]
+
+    while c in char_set:
+        char_set.remove(s[i])
+        i += 1
+    
+    char_set.add(c)
+    max_total = max(max_total,j-i+1)
+    j += 1
+print(max_total)

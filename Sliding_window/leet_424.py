a3ecd3b170b2388c4c4d5369be55d530226b96c9
plearning2001 <p.learning2001@gmail.com>
2026-01-01 17:20:07 +0530

    longest-repeating-character-replacement

---
+++ Sliding_window/leet_424.py
@@ -0,0 +1,19 @@
+s = "AABABBA"
+k = 1
+
+left = 0
+freq = {}
+max_lenth = 0
+max_freq = 0
+
+for right in range(len(s)):
+    char = s[right]
+    freq[char] = freq.get(char,0) + 1
+    max_freq = max(max_freq,freq[char])
+
+    while (right-left+1) - max_freq > k:
+        freq[s[left]] -= 1
+        left += 1
+
+    max_lenth = max(max_lenth,right-left+1)
+print(max_lenth)

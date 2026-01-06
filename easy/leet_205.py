f0a1cc37f060dc44b92c2404459a940b981e623e
plearning2001 <p.learning2001@gmail.com>
2026-01-06 23:59:38 +0530

    isomorphic String alternate method

--- easy/leet_205.py
+++ easy/leet_205.py
@@ -1,26 +1,43 @@
 # isomorphic String 
 
-s = "egg"
-t = "add"
+
+s = "foo"
+t = "bar"
 
 s = "badc"
 t = "baba"
 
-i = 0
+s = "egg"
+t = "add"
 
-dict = {}
+i = 0
+s_set = set()
+t_set = set()
 isomorphic = True
 
 while i < len(s):
-    if s[i] in dict.keys():
-        if dict[s[i]] != t[i]:
-            isomorphic = False
-            break
-    else:
-        dict[s[i]] = t[i]
+    s_set.add(s[i])
+    t_set.add(t[i])
     i += 1
-values_len = len(set(dict.values()))
-if values_len != len(dict.keys()):
+if len(s_set) != len(t_set):
     isomorphic = False
-
 print(isomorphic)
+
+
+
+# dict = {}
+# isomorphic = True
+
+# while i < len(s):
+#     if s[i] in dict.keys():
+#         if dict[s[i]] != t[i]:
+#             isomorphic = False
+#             break
+#     else:
+#         dict[s[i]] = t[i]
+#     i += 1
+# values_len = len(set(dict.values()))
+# if values_len != len(dict.keys()):
+#     isomorphic = False
+
+# print(isomorphic)

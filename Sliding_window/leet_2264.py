cef172c535f9d24d55d77097469fb3a2cf1e7a3b
plearning2001 <p.learning2001@gmail.com>
2026-01-10 23:26:48 +0530

    slid window approach

--- Sliding_window/leet_2264.py
+++ Sliding_window/leet_2264.py
@@ -2,25 +2,21 @@ import sys
 
 
 num = "1221000"
-i = 0
+left = 0
+right = 2
+
 first = num[0]
 second = num[1]
 third = num[2]
 highest = -sys.maxsize
 
-if first == second == third:
-    if int(first+second+third) > int(highest):
-        highest = first+second+third
-while i <= len(num)-3:
-    first = num[i]
-    second = num[i+1]
-    third = num[i+2]
+my_map = set([first,second,third])
 
-    if first == second == third:
-        if int(first+second+third) > int(highest):
-            highest = first+second+third
 
-    i += 1
-if highest == -sys.maxsize:
-    print("")
+for right in range(2,len(num)):
+    if len(set(num[left:right+1])) == 1 and int(str(num[right])*3) > int(highest):
+        highest = str(num[right])*3
+
+    left += 1
+
 print(highest)

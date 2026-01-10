9a0512f8b3282efb3d8aa132d6f67fbdcf7c032d
plearning2001 <p.learning2001@gmail.com>
2026-01-10 23:36:02 +0530

    another approach

--- Sliding_window/leet_2264.py
+++ Sliding_window/leet_2264.py
@@ -3,18 +3,23 @@ import sys
 
 num = "1221000"
 left = 0
-right = 2
-
-first = num[0]
-second = num[1]
-third = num[2]
 highest = -sys.maxsize
-
-my_map = set([first,second,third])
-
 
 for right in range(2,len(num)):
     if len(set(num[left:right+1])) == 1 and int(str(num[right])*3) > int(highest):
+        highest = str(num[right])*3
+
+    left += 1
+
+print(highest)
+
+# ###########################################
+
+left = 0
+highest = -sys.maxsize
+
+for right in range(2,len(num)):
+    if num[right] == num[right-1] == num[right-2]:
         highest = str(num[right])*3
 
     left += 1

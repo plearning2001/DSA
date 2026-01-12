d7c9c9fb11ed2ca5a8bc11a6765839a0e8ad913f
plearning2001 <p.learning2001@gmail.com>
2026-01-13 00:41:27 +0530

    leetcode 252 meeting-schedule

---
+++ interval_pattern/leet_252.py
@@ -0,0 +1,24 @@
+intervals = [(0,30),(5,10),(15,20)]
+if not intervals:
+    print(True)
+final_list = []
+sorted_list = sorted(intervals, key=lambda x: x.start)
+
+final_list.append(sorted_list[0])
+status = True
+
+for l in range(1,len(sorted_list)):
+
+    cur = sorted_list[l]
+
+    prev = final_list[len(final_list)-1]
+
+
+    if prev.end<=cur.start:
+        final_list.append(cur)
+        
+    else:
+        status = False
+        break
+    
+print(status)

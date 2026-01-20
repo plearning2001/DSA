8e768e431c504b415336cc40f1fbf5caa1a29bcd
plearning2001 <p.learning2001@gmail.com>
2026-01-20 23:20:17 +0530

    Comment

--- Sliding_window/leet_3.py
+++ Sliding_window/leet_3.py
@@ -11,10 +11,12 @@ while j < len(s):
     c = s[j]
 
     while c in char_set:
+        #Keep removing from left
         char_set.remove(s[i])
         i += 1
     
     char_set.add(c)
+    # compare with max and current window length
     max_total = max(max_total,j-i+1)
     j += 1
 print(max_total)

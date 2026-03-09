ac756c04c5b50ad38cc6d7777e910eadb7560a12
plearning2001 <p.learning2001@gmail.com>
2026-03-09 23:12:25 +0530

    Intersection

--- interval_pattern/practice_interval.py
+++ interval_pattern/practice_interval.py
@@ -5,9 +5,24 @@ def merge_two(a, b):
     end   = max(a[1], b[1])   # latest end
     return [start, end]
 
-print(merge_two([2, 6], [4, 9]))
-print(merge_two([1, 5], [3, 4]))
-print(merge_two([1, 4], [4, 8]))
+# print(merge_two([2, 6], [4, 9]))
+# print(merge_two([1, 5], [3, 4]))
+# print(merge_two([1, 4], [4, 8]))
+
+
+# Intersection
+
+def intersect(a, b):
+    start = max(a[0], b[0])   # earliest start
+    end   = min(a[1], b[1])   # latest end
+    if start <= end:
+        return [start, end]
+    else:
+        return None
+
+print(intersect([2, 8], [5, 10]))
+print(intersect([1, 4], [6, 9]))
+print(intersect([1, 7], [3, 5]))
 
 
 def intersect(a, b):

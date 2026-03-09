d0d2ddad39e30a4c3cb126b0ddaf07525a72ffb4
plearning2001 <p.learning2001@gmail.com>
2026-03-09 23:08:39 +0530

    Merged interval

--- interval_pattern/practice_interval.py
+++ interval_pattern/practice_interval.py
@@ -1,3 +1,14 @@
+# Merged interval = [min of starts, max of ends]
+
+def merge_two(a, b):
+    start = min(a[0], b[0])   # earliest start
+    end   = max(a[1], b[1])   # latest end
+    return [start, end]
+
+print(merge_two([2, 6], [4, 9]))
+print(merge_two([1, 5], [3, 4]))
+print(merge_two([1, 4], [4, 8]))
+
 
 def intersect(a, b):
     start = max(a[0], b[0])   # latest start
@@ -8,6 +19,6 @@ def intersect(a, b):
     return None   # no intersection
 
 
-print(intersect([2, 8], [5, 10])) 
-print(intersect([1, 4], [6, 9]))  
-print(intersect([1, 7], [3, 5]))  
+# print(intersect([2, 8], [5, 10])) 
+# print(intersect([1, 4], [6, 9]))  
+# print(intersect([1, 7], [3, 5]))  

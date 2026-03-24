13648d832194a485329b0baf84d90459a8d20d38
plearning2001 <p.learning2001@gmail.com>
2026-03-24 23:05:36 +0530

    lambda

--- lambda_function.py
+++ lambda_function.py
@@ -15,7 +15,6 @@ print(add(3, 5))
 square = lambda x: x * x
 print(square(4))
 
-'''
 # Lambda inside a function as a parameter
 def apply_operation(x, y, operation):
     return operation(x, y)
@@ -23,3 +22,23 @@ def apply_operation(x, y, operation):
 result = apply_operation(5, 3, lambda a, b: a + b)
 
 print(result)
+
+'''
+
+
+# 1. Import the reduce function
+from functools import reduce
+
+# 2. Define a standard function that takes two arguments
+def prod(x, y):
+  """Returns the product of two numbers."""
+  return x + y
+
+# 3. Create a list of numbers
+list1 = [1, 2, 3, 4]
+
+# 4. Use reduce with the named function
+product_result = reduce(prod, list1)
+
+# 5. Print the result
+print("Sum:", product_result)
